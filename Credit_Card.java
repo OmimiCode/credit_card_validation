88b2010cf9da5b7cd43c7efa248f6245cf7e7348
Oluwanishola227639 <barnabas.oretan@gmail.com>
2021-02-03 09:15:25 +0100

    chapter 6 Exercises

--- Credit_Card.java
+++ Credit_Card.java
@@ -2,6 +2,7 @@ package Credit_Card_Number_Validation;
 
 public class  Credit_Card {
 
+
     public boolean isValid(long credit_card_number) {
         if ((Long.toString(credit_card_number).length() >= 13
                 && Long.toString(credit_card_number).length() <= 16)
@@ -13,18 +14,25 @@ public class  Credit_Card {
         ) {
             return true;
         } else {
-            throw new IllegalArgumentException("credit card number must have between 13 and 16 digits " +
-                    "and the number must start with 4 for Visa cards," +
-                    "5 for MasterCard Credit cards" +
-                    "6 for Discover cards " +
-                    "and 37 for American Express cards");
+            throw new IllegalArgumentException("""
+                    credit card number must have between 13 and 16 digits\s
+                    and the number must start with 4 for Visa cards,
+                    5 for MasterCard Credit cards,
+                    6 for Discover cards\s
+                    and 37 for American Express cards""");
         }
     }
 
     public int sumOfDoubleEvenPlace(long credit_card_number) {
         int sumOfDoubledEvenPlace = 0;
-        for (int counter =1; counter <= Long.toString(credit_card_number).length(); counter++) {
-            int lastDigit = (int) getLastDigit(credit_card_number);
+        int counterLimit = Long.toString(credit_card_number).length();
+        sumOfDoubledEvenPlace = loopThroughCreditCardNumberForSumOfEvenPlaceDigits(credit_card_number, sumOfDoubledEvenPlace, counterLimit);
+        return sumOfDoubledEvenPlace;
+    }
+
+    private int loopThroughCreditCardNumberForSumOfEvenPlaceDigits(long credit_card_number, int sumOfDoubledEvenPlace, int counterLimit) {
+        for (int counter = 1; counter <= counterLimit; counter++) {
+            int lastDigit = getLastDigit(credit_card_number);
             sumOfDoubledEvenPlace = ifNumberIsAtEvenPlace(sumOfDoubledEvenPlace, counter, lastDigit);
             credit_card_number = truncateLastDigit(credit_card_number);
         }
@@ -40,17 +48,15 @@ public class  Credit_Card {
         return credit_card_number;
     }
 
-    public int ifNumberIsAtEvenPlace(int sumOfDoubledEvenPlace, int counter, int lastDigit) {
+    private int ifNumberIsAtEvenPlace(int sumOfDoubledEvenPlace, int counter, int lastDigit) {
         if (counter % 2 == 0) {
            lastDigit *=2;
-            sumOfDoubledEvenPlace = ifDoubledLastDigitIsTwoDigit(sumOfDoubledEvenPlace, lastDigit);
-        }else{
-            sumOfDoubledEvenPlace += 0;
+            sumOfDoubledEvenPlace = ifDoubledLastDigitIsTwoDigitForEvenPlaces(sumOfDoubledEvenPlace, lastDigit);
         }
         return sumOfDoubledEvenPlace;
     }
 
-    private int ifDoubledLastDigitIsTwoDigit(int sumOfDoubledEvenPlace, int lastDigit) {
+    private int ifDoubledLastDigitIsTwoDigitForEvenPlaces(int sumOfDoubledEvenPlace, int lastDigit) {
         if (lastDigit >= 10) {
             sumOfDoubledEvenPlace += lastDigit - 9;
         } else {
@@ -60,7 +66,35 @@ public class  Credit_Card {
     }
 
 
+    public int sumOfOddPlace(long credit_card_number) {
+        int sumOfOddPlace = 0;
+        int counterLimit = Long.toString(credit_card_number).length();
+        sumOfOddPlace = loopThroughCreditCardNumberForSumOfOddPlaceDigits(credit_card_number, sumOfOddPlace, counterLimit);
+        return sumOfOddPlace;
     }
+
+    private int loopThroughCreditCardNumberForSumOfOddPlaceDigits(long credit_card_number, int sumOfOddPlace, int counterLimit) {
+        for (int counter = 1; counter <= counterLimit; counter++) {
+            int lastDigit = getLastDigit(credit_card_number);
+            sumOfOddPlace = ifNumberIsAtOddPlace(sumOfOddPlace, counter, lastDigit);
+            credit_card_number = truncateLastDigit(credit_card_number);
+        }
+        return sumOfOddPlace;
+    }
+
+
+    private int ifNumberIsAtOddPlace(int sumOfOddPlace, int counter, int lastDigit) {
+        if (counter % 2 != 0) {
+            sumOfOddPlace += lastDigit;
+        }
+        return sumOfOddPlace;
+    }
+
+
+    public int addSumOfDoubledEvenPlaceAndSumOfOddPlace(int sumOfDoubledEvenPlace, int sumOfOddPlace) {
+        return sumOfDoubledEvenPlace + sumOfOddPlace;
+    }
+}
 
 
 
